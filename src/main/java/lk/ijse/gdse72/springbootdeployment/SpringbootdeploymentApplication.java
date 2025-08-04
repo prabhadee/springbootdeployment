d587556873a5714c7f17b6dfb846acf14ccddfd2
Prabhadee Paranavithana <pprabhadee@gmail.com>
2025-08-04 10:06:21 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse72/springbootdeployment/SpringbootdeploymentApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse72.springbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/gdse72/springbootdeployment/SpringbootdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse72.springbootdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
