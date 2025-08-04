f5236f13d6f5594e9d220115231be328cfff25db
Prabhadee Paranavithana <pprabhadee@gmail.com>
2025-08-04 10:28:14 +0530

    add a java version

---
+++ src/main/java/lk/ijse/gdse72/springbootdeployment/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse72.springbootdeployment.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    @GetMapping
+    public String getCustomer() {
+        return "Customer 1";
+    }
+}
